d63a268235330add5fc3f684af4cb60920dacf24
Marcelo Batista <marcelo.25batista@gmail.com>
2023-09-10 23:32:34 -0300

    Meu primeiro repositório

---
+++ Fundamentos.php
@@ -0,0 +1,27 @@
+<?php
+
+echo"Meu primeiro código php! <strong>Hello World</strong>";
+
+/*
+
+*Para acessar o servidor local, digite no navegador "localhost/".
+
+*GIT   usuário: marcelo-abatista  Senha: Olecarm!0
+
+*Para clonar repositório em uma pasta, com o botão direito selecione "Open Git Bash Here". Após abrir a linha de comando, digite "git clone link_do_repositório" e cloque no botão "Enter".	 Para baixar uma pasta de repositório, já renomeando a pasta, digite "git clone link_do_repositório nome_da_pasta"
+
+*BitBucket: É parecido com o GitHub, porém, é possível criar repositóriso privados para até 5 pessoas, de forma gratuita. O GitHub permite criar repositórios privados por uma mensalidade aproximada de US$7,00.
+
+*Ao criar um repositório,  ha a opção de "Add .gitignore", onde se informa algum item da pasta que não quer que suba para o repositório, como por exemplo o composer (dependências).
+
+*Após criado o repositório no GitHub, vá até a pasta qu deseja que suba para o repositório, copie o endereço dela, e em seguida, no cmd, digite cd e cole o endereço da pasta. Na sequência, digite "git remote add origin url_copiada_do_repositório"
+
+git init
+git remote add origin https://github.com/marcelo-abatista/phphc.git
+git config --global user.name "Marcelo Batista"
+git config --global user.email "marcelo.25batista@gmail.com"
+git push origin master
+
+*/
+
+?>
